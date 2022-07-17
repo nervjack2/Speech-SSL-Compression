2db3af3ea8110e30a3f95eba3e6fdb95c4b0a3fa
Hao Tang <hao.tang@ed.ac.uk>
2022-07-17 19:40:56 +0100

    Avoid += as it is an in-place operator in pytorch 1.12

--- melhubert/module.py
+++ melhubert/module.py
@@ -229,10 +229,10 @@ class TransformerEncoder(nn.Module):
     def extract_features(self, x, padding_mask=None, attn_mask=None, get_hidden=False):
         if padding_mask is not None:
             x[padding_mask] = 0
-    
+
         x_conv = self.pos_conv(x.transpose(1, 2))
         x_conv = x_conv.transpose(1, 2)
-        x += x_conv
+        x = x + x_conv
 
         if not self.layer_norm_first:
             x = self.layer_norm(x)
