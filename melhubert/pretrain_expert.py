58baff0e8901dc77e58f4897ad7e4c59a088473a
林子權 <nervjack2@login.speech>
2023-07-07 23:58:09 +0800

    allow loading from any checkpoints

--- melhubert/pretrain_expert.py
+++ melhubert/pretrain_expert.py
@@ -38,14 +38,40 @@ class MelHuBERTPretrainer(nn.Module):
 
         # Do initialization from a checkpoint if needed
         if self.initial_weight:
-            if "weight-pruning" in self.initial_weight:
-                # Initialize the pruning mask if needed 
+            all_states = torch.load(self.initial_weight, map_location="cpu")
+            loaded_mode = all_states['Args'].mode
+        
+            # If the attention heads have been pruned 
+            if 'Pruned_heads' in all_states:
+                self.pruned_heads = all_states["Pruned_heads"]
+                summarized = {}
+                for layer_heads in self.pruned_heads:
+                    for layer in layer_heads:
+                        summarized[layer] = summarized.get(layer, 0) + len(layer_heads[layer])
+                pruned_heads = summarized
+
+                for idx, layer in enumerate(self.model.encoder.layers):
+                    if idx in pruned_heads:
+                        layer.self_attn.num_heads -= pruned_heads[idx]
+                        orig_embed_dim = layer.self_attn.embed_dim
+                        embed_dim = layer.self_attn.head_dim * layer.self_attn.num_heads
+                        bias = True
+                        layer.self_attn.embed_dim = embed_dim
+                        layer.self_attn.k_proj = nn.Linear(orig_embed_dim, embed_dim, bias=bias)
+                        layer.self_attn.v_proj = nn.Linear(orig_embed_dim, embed_dim, bias=bias)
+                        layer.self_attn.q_proj = nn.Linear(orig_embed_dim, embed_dim, bias=bias)
+                        layer.self_attn.out_proj = nn.Linear(embed_dim, orig_embed_dim, bias=bias)
+                        layer.self_attn.skip_embed_dim_check = True
+                        layer.self_attn.reset_parameters()
+            
+            # Initialize the pruning mask if needed 
+            # (Do not support executing other compression methods on a weight pruned checkpoint)
+            if 'Pruning' in all_states:
                 params_to_prune, _ = get_params_to_prune(self.model)
                 prune.global_unstructured(
                     params_to_prune,
                     pruning_method=prune.Identity,
                 )
-            all_states = torch.load(self.initial_weight, map_location="cpu")
             try:             
                 self.model.load_state_dict(all_states["model"])
                 print(f'[Pretrainer] Load initilization model weight from {self.initial_weight}')
@@ -62,6 +88,8 @@ class MelHuBERTPretrainer(nn.Module):
     def add_state_to_save(self, all_states):
         all_states['model'] = self.model.state_dict() if not self.multi_gpu else self.model.module.state_dict()
         all_states['Upstream_Config'] = self.upstream_config
+        if self.pruned_heads:
+            all_states["Pruned_heads"] = self.pruned_heads
         return all_states
 
     def forward(self, data, global_step=0, log_step=1000):
